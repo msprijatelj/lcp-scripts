ef1770d60af1503544d899d4416eec601bd1de61
msprijatelj <44978587+msprijatelj@users.noreply.github.com>
2022-04-26 22:52:07 -0400

    add license_id-generating script

---
+++ modify_lcp.py
@@ -0,0 +1,33 @@
+import json
+import sys
+
+def add_parent_id(data, frame_map):
+    for d in data:
+        license_id = ""
+        license_name = d.get('license')
+        if license_name and license_name != 'GMS' and d.get('source'):
+            license_id = frame_map[license_name]
+        elif variant_name := d.get('variant'):
+            license_id = frame_map[variant_name]
+        elif "mechtype" in d:
+            license_id = d['id']
+        d["license_id"] = license_id
+
+def add_parent_ids_to_file(filename, frame_file):
+    with open(filename, 'r+', encoding='utf-8') as f:
+        data = json.load(f)
+    
+    with open(frame_file, 'r+', encoding='utf-8') as f:
+        frame_data = json.load(f)
+    
+    frame_map = {f['name']: f['id'] for f in frame_data}
+    add_parent_id(data, frame_map)
+
+    with open(filename, 'w+', encoding='utf-8') as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    frame_fn = sys.argv[1]
+    fn = sys.argv[2]
+    add_parent_ids_to_file(fn, frame_fn)
